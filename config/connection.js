e190e4d48ade1a751239542e49f0047a36c17543
DMilligan <dmilligan@springlocker.com>
2018-08-04 14:15:49 -0400

    modifying connection.js to use jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,30 @@
 // ==============================================================================
-// Set Dependencies
+// Set Dependencies & Global Variables
 // ==============================================================================
 
 var mysql = require('mysql');
+var connection;
 
 // ==============================================================================
 // Establish Database Connection
 // ==============================================================================
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'creator_db'
-});
+if (process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'creator_db'
+    });
+}
+
+
 
 // ==============================================================================
 // Connect to Database
